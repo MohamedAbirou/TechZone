166bcdd9a4d9cb0c9eec3036b0adeb66d3e0ef47
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-12-14 16:57:54 +0100

    Feat: display related products if available

--- src/app/(pages)/products/[slug]/page.tsx
+++ src/app/(pages)/products/[slug]/page.tsx
@@ -40,27 +40,29 @@ export default async function Product({ params: { slug } }) {
     <>
       <ProductHero product={product} />
       {product?.enablePaywall && <PaywallBlocks productSlug={slug as string} disableTopPadding />}
-      <Blocks
-        disableTopPadding
-        blocks={[
-          {
-            blockType: 'relatedProducts',
-            blockName: 'Related Product',
-            relationTo: 'products',
-            introContent: [
-              {
-                type: 'h3',
-                children: [
-                  {
-                    text: 'Related Products',
-                  },
-                ],
-              },
-            ],
-            docs: relatedProducts,
-          },
-        ]}
-      />
+      {relatedProducts && (
+        <Blocks
+          disableTopPadding
+          blocks={[
+            {
+              blockType: 'relatedProducts',
+              blockName: 'Related Product',
+              relationTo: 'products',
+              introContent: [
+                {
+                  type: 'h3',
+                  children: [
+                    {
+                      text: 'Related Products',
+                    },
+                  ],
+                },
+              ],
+              docs: relatedProducts,
+            },
+          ]}
+        />
+      )}
     </>
   )
 }
